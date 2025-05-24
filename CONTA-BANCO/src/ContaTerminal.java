de9ca790f53cc9c089dc20eab8e4852a0c8c2a16
Kayo Santos <kayorodrigodzn@gmail.com>
2025-05-24 03:13:34 -0300

    pronto

--- CONTA-BANCO/src/ContaTerminal.java
+++ CONTA-BANCO/src/ContaTerminal.java
@@ -1,3 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o número da sua conta: ");
+        int numeroConta = scanner.nextInt();
+        
+        System.out.println("Digite o número da agência: ");
+        int numeroAgencia = scanner.nextInt();
+        
+        System.out.println("Digite o seu nome: ");
+        String nomeTitular = scanner.next();
+        
+        System.out.println("Digite o saldo inicial: ");
+        double saldo = scanner.nextDouble();
+        
+        System.out.println("Conta criada com sucesso!");
+        System.out.println("Número da conta: " + numeroConta);
+        System.out.println("Agência: " + numeroAgencia);
+        System.out.println("Titular: " + nomeTitular);
+        System.out.println("Saldo: R$ " + saldo);
+        
+    }
+
 }
